7336dfbfc13e64f8f98b4740402d7d5291b6ec0c
TheKingOfEngland <AliveSheCried@users.noreply.github.com>
2021-02-20 11:47:18 +0000

    ex-7 completed

--- ex-7/resources/script/script.js
+++ ex-7/resources/script/script.js
@@ -1,9 +1,32 @@
+//buttons
 const incr = document.getElementById('incr');
 const decr = document.getElementById('decr');
+// fields to populate
 const pic = document.getElementById('car');
-const images = ['car-1.png', 'car-2.png', 'car-3.png', 'car-4.png', 'car-5.png'];
+const txt = document.getElementById('txt');
+const client = document.getElementById('client');
+// img path
 const path = './resources/images/'
-let limit = images.length;
+
+//Class for testimonial
+class Testimonial {
+    constructor(client, desc, img) { 
+      this.client = client;
+      this.desc = desc;
+      this.img = img;
+    }
+   };
+ // Array of testimonials to loop through 
+  const testimonials = [
+    new Testimonial('ABC', 'Javascript Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation', 'car-1.png'),
+    new Testimonial('DEF', 'Rest API Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation', 'car-2.png'),
+    new Testimonial('GHI', 'Openbanking Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation', 'car-3.png'),
+    new Testimonial('JKL', 'Node.js Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation', 'car-4.png'),
+    new Testimonial('MNO', 'Something else Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation', 'car-5.png'),
+  ];
+
+//loooping constraints
+let limit = testimonials.length;
 let position = 0;
 
 incr.addEventListener('click', function() {
@@ -11,14 +34,19 @@ incr.addEventListener('click', function() {
     if (position === limit) {
         position = 0;
     } 
-    pic.src = `${path}${images[position]}`;  
+    //assigning values to fields
+    pic.src = `${path}${testimonials[position].img}`; 
+    client.textContent = testimonials[position].client;
+    txt.textContent = testimonials[position].desc; 
 }, false);
 
 decr.addEventListener('click', function() {       
     if (position < 0) {
         position = limit - 1;
     } 
-    pic.src = `${path}${images[position]}`; 
+    pic.src = `${path}${testimonials[position].img}`; 
+    client.textContent = testimonials[position].client;
+    txt.textContent = testimonials[position].desc; 
     position --;  
 }, false);
 
