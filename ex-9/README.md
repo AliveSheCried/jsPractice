1b76fcf0e94a5d33b38041288888212c9831c846
TheKingOfEngland <AliveSheCried@users.noreply.github.com>
2021-03-14 19:30:49 +0000

    ex-9 extension

--- ex-9/README.md
+++ ex-9/README.md
@@ -6,8 +6,11 @@
 - [Technologies used](#technologies-used)
 
 ## Project details
-This project involves wiring up two different sections. The first section is the filter buttons, whose purposes are to filter the store items by class. The second section is the search filter, whose purpose is to filter the store items by a search. 
-[Refer to exercise page for full details](https://jsbeginners.com/filter-project/)
+This project involves wiring up two different sections: 
+1. Searching, specifcally using filter buttons, whose purposes are to filter the store items by class, and the search filter, whose purpose is to filter the store items by a search. 
+2. Allows the user to click on an image and scroll through them using a modal window. **Note** CSS and HTML for this were downloaded, not created, and there is much of it; for this exercise, I appended my own CSS to the main file and didn't follow the exist conventions, whatever they were, as it would've taken longer to understand what was going on here than to write all the code for th exercise.
+[Refer to exercise page 1 for the  for full details](https://jsbeginners.com/filter-project/)
+[Refer to exercise page 2 for the  for full details](https://jsbeginners.com/modal-javascript-project/)
 
 
 ## Technologies used
--- ex-9/js/app.js
+++ ex-9/js/app.js
@@ -1,55 +1,108 @@
-const types = ['all', 'cakes', 'cupcakes', 'sweets', 'doughnuts'];
+//code to collect DOM elements
+//buttons
+const types = document.querySelectorAll('.btn');
+//each store item
 const toChange = document.querySelectorAll(`[data-item]`);
+//search bar
+const searchParam = document.querySelector('#search-item');
 
-//function to process button clicks
-const filterButton = (e) => {    
-    const button = e.target.getAttribute('data-filter');
-    const status = document.querySelector(`[data-item=${CSS.escape(button)}]`)  
-    function showHide(button) {
-        if (status.style.display === 'none') {
-            const toShow = document.querySelectorAll(`[data-item=${CSS.escape(button)}]`);           
-            toShow.forEach(x => x.style.display = 'block');           
-        };
-        types.forEach(type => {
-            if (type != button) {
-                const toHide = document.querySelectorAll(`[data-item=${CSS.escape(type)}]`);           
-                toHide.forEach(x => x.style.display = 'none'); 
-            } 
+//code for filter buttons
+types.forEach(button => {
+    button.addEventListener('click', (e)=>{
+        const filter = e.target.dataset.filter;
+        toChange.forEach(item => {
+            if (filter === 'all') {
+                item.style.display = 'block'
+            } else {
+                if (item.classList.contains(filter)){
+                    item.style.display = 'block'
+                } else {
+                    item.style.display = 'none'
+                }
+            }            
         });
-    }
-    switch(button) {
-        case 'all':
-            toChange.forEach(x => x.style.display = 'block');
-        break;   
-        case 'cakes':
-            showHide('cakes');
-        break;  
-        case 'cupcakes':            
-            showHide('cupcakes');
-        break;  
-        case 'sweets':            
-            showHide('sweets');
-        break;
-        case 'doughnuts':            
-            showHide('doughnuts');
-        break;
-    }
-};
 
-// event listener for buttons
-const pressedButton = document.getElementsByClassName('btn btn-outline-secondary btn-black text-uppercase filter-btn m-2');
-for (let i = 0; i < pressedButton.length; i++) {
-    pressedButton[i].addEventListener('click', filterButton);
-};
+    })
+});
 
-//event listener for search bar
-const searchParam = document.getElementById('search-icon');
-let searchParamValue = document.getElementById('search-item');
-
-searchParam.addEventListener('click', function() {
-    searchParamValue = searchParamValue.value;
-    console.log(searchParamValue)
-})
+//code for search bar
+searchParam.addEventListener('keyup', function(e) {
+    const filter = e.target.value;    
+    toChange.forEach(item => {
+        if (filter === 'all') {
+            item.style.display = 'block'
+        } else {
+            if (item.classList.contains(filter)){
+                item.style.display = 'block'
+            } else {
+                item.style.display = 'none'
+            }
+        }       
+    });
+}, true)
 
 
+//code for modal
+const modalWindow = document.getElementById('modal');
+const modalImg = document.getElementById('modal-img');
+const modalButtons = document.querySelectorAll('[class^="modal-btn"]');
+const images = document.querySelectorAll('[class^="card-img"]');
 
+images.forEach((image, index) => {
+    const imgIndex = index;
+    image.addEventListener('click', (e) => {
+        const selImg = `url("${e.target.getAttribute('src')}")`;       
+        modalImg.style.backgroundImage = selImg;
+        modalWindow.style.display = 'block'; 
+
+        // button eventlistener
+        modalButtons.forEach(btn => {
+        btn.addEventListener('click', (e)=> {
+        const selBtn = e.target.classList;  
+        selBtn.forEach(cls => {
+            if (cls.includes('close')) {                
+                modalWindow.style.display = 'none'
+                  }  
+              });   
+             })
+        });
+        // ends
+    })
+    
+});
+
+
+
+
+//Original code for filter buttons - worked, but not svelte
+//function to show/hide tiles for selected/entered item
+// function showHide(button) {
+//     if (button === 'all') {
+//         toChange.forEach(x => x.style.display = 'block');
+//     } else {
+//         const status = document.querySelector(`[data-item=${CSS.escape(button)}]`) 
+//         if (status.style.display === 'none') {
+//             const toShow = document.querySelectorAll(`[data-item=${CSS.escape(button)}]`);           
+//             toShow.forEach(x => x.style.display = 'block');           
+//         };
+//         types.forEach(type => {
+//             if (type != button) {
+//                 const toHide = document.querySelectorAll(`[data-item=${CSS.escape(type)}]`);           
+//                 toHide.forEach(x => x.style.display = 'none'); 
+//             } 
+//         })
+//     };    
+// }
+// //function to process button clicks
+// const filterButton = (e) => {  
+//     //which buttton is pressed  
+//     const button = e.target.getAttribute('data-filter');
+//     //pass value to show hide        
+//     showHide(button);
+// };
+
+// // event listener for buttons
+// const pressedButton = document.getElementsByClassName('btn btn-outline-secondary btn-black text-uppercase filter-btn m-2');
+// for (let i = 0; i < pressedButton.length; i++) {
+//     pressedButton[i].addEventListener('click', filterButton);
+// };
